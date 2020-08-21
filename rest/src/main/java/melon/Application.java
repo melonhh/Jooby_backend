d66016ccffd357eb4e9fa3daeff0f308e920d684
melonhh <1603871134@qq.com>
2020-08-21 13:21:53 +0800

    Jenkins测试

--- rest/src/main/java/melon/Application.java
+++ rest/src/main/java/melon/Application.java
@@ -6,27 +6,27 @@ import io.jooby.Route;
 public class Application extends Jooby {
     {
         // 接收一个handler，产生一个handler
-//        decorator(next -> ctx -> {
-//            long start = System.currentTimeMillis();
-//
-//            Object response = next.apply(ctx);
-//
-//            long end = System.currentTimeMillis();
-//            long took = end - start;
-//
-//            System.out.println("took:" + took + "ms");
-//
-//            return response;
+        decorator(next -> ctx -> {
+            long start = System.currentTimeMillis();
+
+            Object response = next.apply(ctx);
+
+            long end = System.currentTimeMillis();
+            long took = end - start;
+
+            System.out.println("took:" + took + "ms");
+
+            return response;
+        });
+
+//        before(ctx -> {
+//           ctx.setResponseHeader("Server", "Jooby");
 //        });
-
-        before(ctx -> {
-           ctx.setResponseHeader("Server", "Jooby");
-        });
-
-        after((ctx, result, failure) -> {
-            System.out.println(result);
-            ctx.setResponseHeader("foo", "bar");
-        });
+//
+//        after((ctx, result, failure) -> {
+//            System.out.println(result);
+//            ctx.setResponseHeader("foo", "bar");
+//        });
 
         get("/", ctx -> "decorator");
     }
