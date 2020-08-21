f3a2baf94e9139de7cf71634ab3e52ba5707a46c
melonhh <1603871134@qq.com>
2020-08-21 12:35:07 +0800

    Jenkins测试

--- rest/src/main/java/melon/Application.java
+++ rest/src/main/java/melon/Application.java
@@ -28,7 +28,7 @@ public class Application extends Jooby {
             ctx.setResponseHeader("foo", "bar");
         });
 
-        get("/", ctx -> "decorator");
+        get("/hello", ctx -> "decorator");
     }
 
     public static void main(String[] args) {
